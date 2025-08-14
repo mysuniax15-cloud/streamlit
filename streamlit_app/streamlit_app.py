630388d58a63fac830e7b1bc7503152350e37c15
mysuniax15-cloud <mysuniax15@gmail.com>
2025-08-14 22:31:18 +0900

    Update streamlit_app.py

--- streamlit_app/streamlit_app.py
+++ streamlit_app/streamlit_app.py
@@ -68,11 +68,10 @@ st.set_page_config(page_title="리더십 분석기", layout="wide")
 
 
 # ====== A.X 4.0 API Key 설정 ======
-API_KEY = st.secrets.get("SKT_AX4_KEY") or os.getenv("SKT_AX4_KEY")
+SKT_AX4_KEY = "sktax-XyeKFrq67ZjS4EpsDlrHHXV8it"
 client = OpenAI(
     base_url="https://guest-api.sktax.chat/v1",
-    api_key=API_KEY,
-    timeout=60,
+    api_key=SKT_AX4_KEY
 )
 
 
@@ -3955,4 +3954,5 @@ else:
 
 # 실행 안내
 # streamlit run ax4_final.py
+
 
